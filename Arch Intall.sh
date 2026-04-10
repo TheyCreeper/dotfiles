a8e7380e196bd989f6b19d8fdb7f670a53d5c5ca
TheyCreeper <theycreeper@proton.me>
2026-04-10 12:51:03 -0400

    feat: stuff

--- Arch Intall.sh
+++ Arch Intall.sh
@@ -1,5 +1,5 @@
 # Install wanted arch packages
-sudo pacman -Syu --needed - < arch_pkgslist.txt
+yay -Syu --needed --noconfirm - < arch_pkgslist.txt
 
 # Install gnome settings
 ./dot_config/gnome/install.sh
