370cdb8d930eefcf6ff0a3fcd716a5837a93e8e5
TheyCreeper <theycreeper@proton.me>
2026-04-21 15:34:58 -0400

    feat: stuff

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,3 +2,4 @@ if status is-interactive
     command fastfetch --logo-type small --logo arch
     set fish_greeting ""
 end
+set -gx PATH $PATH /home/theycreeper/.dotnet/tools
---
+++ dot_config/private_fish/functions/sysup.fish
@@ -0,0 +1,3 @@
+function sysup --description 'alias sysup=yay -Syu --no-confirm'
+    command yay -Syu --noconfirm
+end
