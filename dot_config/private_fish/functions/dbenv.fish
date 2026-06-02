a49d006b093965324b5ff1adac8a33dae4792c15
TheyCreeper <theycreeper@proton.me>
2026-06-02 16:58:59 -0400

    feat: some changes

---
+++ dot_config/private_fish/functions/dbenv.fish
@@ -0,0 +1,4 @@
+function dbenv --description 'alias dbenv=cd ~/Documents/Dev/cardb-api/cardbapi && dolt sql-server -P 3306'
+    cd ~/Documents/Dev/cardb-api/cardbapi
+    command dolt sql-server -P 3306
+end
