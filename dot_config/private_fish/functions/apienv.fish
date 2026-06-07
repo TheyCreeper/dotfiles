daa8cbfc04dfcb90ec60a91e51fbdf6474c720f3
TheyCreeper <theycreeper@proton.me>
2026-06-07 11:19:33 -0400

    rem: removed old functions

--- dot_config/private_fish/functions/apienv.fish
+++
@@ -1,4 +0,0 @@
-function apienv --description 'alias apienv=cd ~/Documents/Dev/cardb/CarDB_Api && source ~/Documents/Dev/cardb/CarDB_Api/.venv/bin/activate'
-    cd ~/Documents/Dev/cardb/CarDB_Api
-    source ~/Documents/Dev/cardb/CarDB_Api/.venv/bin/activate.fish
-end
